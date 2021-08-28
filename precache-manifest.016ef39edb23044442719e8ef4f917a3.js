a892586c58ac2dbfc21d7143fa93c226ec76ca3e
Yarema Kurakh <jeremaker00@gmail.com>
2021-08-28 13:50:09 +0300

    Updates

---
+++ precache-manifest.016ef39edb23044442719e8ef4f917a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "22389cf47ad284464c13506f2e22e8af",
+    "url": "/sp-react-got-app/static/media/error.22389cf4.jpg"
+  },
+  {
+    "revision": "194ea6aeac4b05acac98",
+    "url": "/sp-react-got-app/static/js/runtime~main.194ea6ae.js"
+  },
+  {
+    "revision": "0918cf85e9b012e2f9e9",
+    "url": "/sp-react-got-app/static/js/main.0918cf85.chunk.js"
+  },
+  {
+    "revision": "fa129324fef6bfddb5d5",
+    "url": "/sp-react-got-app/static/js/1.fa129324.chunk.js"
+  },
+  {
+    "revision": "0918cf85e9b012e2f9e9",
+    "url": "/sp-react-got-app/static/css/main.633ecc24.chunk.css"
+  },
+  {
+    "revision": "fa129324fef6bfddb5d5",
+    "url": "/sp-react-got-app/static/css/1.e3630864.chunk.css"
+  },
+  {
+    "revision": "c081cee4c83011d280ad6dedd7b55a54",
+    "url": "/sp-react-got-app/index.html"
+  }
+];
